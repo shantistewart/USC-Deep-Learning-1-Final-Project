05dcd96ef8fd6afbe7e868f4a77129e8c9f66298
shantistewart <shantiystewart@gmail.com>
2022-05-09 22:44:08 -0700

    Fixed minor indentation error in load_data() method.

--- preprocessing/load_data_class.py
+++ preprocessing/load_data_class.py
@@ -5,20 +5,22 @@ import os
 from scipy.io.wavfile import read
 
 
-# major and minor class labels:
+# major and minor chord class labels:
 MAJ = 0
 MIN = 1
 
 
 class DataLoader:
-    """Class for loading and splitting data into features and labels.
+    """Class for loading data and splitting intro train/test sets.
 
     Attributes:
         N: Total number of data points.
+        test_fract: Fraction of total data to use for test set.
     """
 
-    def __init__(self):
+    def __init__(self, test_fract=0.2):
         self.N = None
+        self.test_fract = test_fract
 
     def load_data(self, data_folder):
         """Loads data.
@@ -27,7 +29,7 @@ class DataLoader:
             data_folder: Path of data folder.
 
         Returns:
-            X: Data.
+            X: Audio data.
                 dim: (N, n_samples)
             y: Labels.
                 dim: (N, )
@@ -38,7 +40,7 @@ class DataLoader:
         y = []
         for root, dirs, _ in os.walk(data_folder, topdown=True):
             for d in dirs:
-                # set label
+                # set label:
                 if d == 'major':
                     label = MAJ
                 elif d == 'minor':
@@ -50,7 +52,6 @@ class DataLoader:
                         X.append(audio_data)
                         y.append(label)
 
-            self.N = len(y)
-
-            return X, y, Fs
+        self.N = len(y)
+        return X, y, Fs
 
