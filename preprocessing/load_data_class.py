bf39d0d07c5d1bca8775572d16c6e67ee0c41b32
shantistewart <shantiystewart@gmail.com>
2022-05-09 17:25:39 -0700

    Started DataLoader class.

---
+++ preprocessing/load_data_class.py
@@ -0,0 +1,28 @@
+"""File containing class for loading data."""
+
+
+class DataLoader:
+    """Class for loading and splitting data into features and labels.
+
+    Attributes:
+        N: Total number of data points.
+    """
+
+    def __init__(self):
+        self.N = None
+
+    def load_data(self, data_folder):
+        """Loads data.
+
+        Args:
+            data_folder: Path of data folder.
+
+        Returns:
+            X: Features.
+                dim: (N, n_samples)
+            y: Labels.
+                dim: (N, )
+        """
+
+        # return X, y
+
